2d19824361c615411ff3978481bd3ae9ab6dd1ac
cocodemon <milanpaul212@outlook.com>
2026-05-06 23:19:23 +0530

    connected chunk reading with tcp listener

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -2,51 +2,52 @@ package main
 
 import (
 	"fmt"
-	"io"
+	"myhttp/internal/request"
+	// "io"
 	"log"
 	"net"
 )
 
-func worker(file io.ReadCloser, ch chan string) {
-	defer close(ch)
-	buffer := make([]byte, 8)
-	var buf []byte
-	for {
-		n, err := file.Read(buffer)
-
-		if n > 0 {
-			for _, b := range buffer[:n] {
-				if b == '\n' {
-					ch <- string(buf)
-					buf = buf[:0]
-				} else {
-					buf = append(buf, b)
-				}
-			}
-		}
-
-		if err == io.EOF {
-			// flush remaining buffer if any
-			if len(buf) > 0 {
-				ch <- string(buf)
-			}
-			return
-		}
-
-		if err != nil {
-			ch <- err.Error()
-			return
-		}
-	}
-}
-
-func readchannel(file io.ReadCloser) <-chan string {
-
-	ch := make(chan string)
-	// return ch
-	go worker(file, ch)
-	return ch
-}
+// func worker(file io.ReadCloser, ch chan string) {
+// 	defer close(ch)
+// 	buffer := make([]byte, 8)
+// 	var buf []byte
+// 	for {
+// 		n, err := file.Read(buffer)
+//
+// 		if n > 0 {
+// 			for _, b := range buffer[:n] {
+// 				if b == '\n' {
+// 					ch <- string(buf)
+// 					buf = buf[:0]
+// 				} else {
+// 					buf = append(buf, b)
+// 				}
+// 			}
+// 		}
+//
+// 		if err == io.EOF {
+// 			// flush remaining buffer if any
+// 			if len(buf) > 0 {
+// 				ch <- string(buf)
+// 			}
+// 			return
+// 		}
+//
+// 		if err != nil {
+// 			ch <- err.Error()
+// 			return
+// 		}
+// 	}
+// }
+//
+// func readchannel(file io.ReadCloser) <-chan string {
+//
+// 	ch := make(chan string)
+// 	// return ch
+// 	go worker(file, ch)
+// 	return ch
+// }
 
 func main() {
 	listen, err := net.Listen("tcp", ":42069")
@@ -61,9 +62,15 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-		out := readchannel(conn)
-		for line := range out {
-			fmt.Println(line)
+		r, err := request.RequestFromReader(conn)
+		if err != nil {
+			log.Println(err)
+			continue
 		}
+		fmt.Printf("Request line:\n- Method: %s\n- Target: %s\n- Version: %s\n",
+			r.RequestLine.Method,
+			r.RequestLine.RequestTarget,
+			r.RequestLine.HttpVersion,
+		)
 	}
 }
