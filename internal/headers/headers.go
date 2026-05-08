a3849b1efd13ef3c683e29aec6407c9125d27381
cocodemon <milanpaul212@outlook.com>
2026-05-08 23:54:12 +0530

    updated key validation

---
+++ internal/headers/headers.go
@@ -0,0 +1,50 @@
+package headers
+
+import (
+	"fmt"
+	"strings"
+	"unicode"
+)
+
+type Headers map[string]string
+
+func NewHeaders() Headers {
+	return make(Headers)
+}
+
+func parseHeadersLine(h Headers, str string) (err error) {
+
+	firstColonIndex := strings.Index(str, ":")
+	if firstColonIndex == -1 {
+		return fmt.Errorf("invlaid header")
+	}
+	key := str[:firstColonIndex]
+	value := str[firstColonIndex+1:]
+	for _, char := range key {
+		if !unicode.IsDigit(char) && !unicode.IsLetter(char) && char != '-' {
+			return fmt.Errorf("invalid Header")
+		}
+	}
+
+	h[strings.ToLower(key)] = strings.Trim(value, " ")
+	return nil
+}
+
+func (h Headers) Parse(data []byte) (n int, done bool, err error) {
+	strData := string(data)
+	firstHeaderEndIndex := strings.Index(strData, "\r\n")
+	if firstHeaderEndIndex == -1 {
+		return 0, false, nil
+	}
+	if firstHeaderEndIndex == 0 {
+		return 2, true, nil
+	}
+	slicedData := strData[:firstHeaderEndIndex]
+	consumedData := len(slicedData) + 2
+	parseErr := parseHeadersLine(h, slicedData)
+	if parseErr != nil {
+		return 0, false, parseErr
+	}
+
+	return consumedData, false, nil
+}
