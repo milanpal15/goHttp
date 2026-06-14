0a93d0e7815ca3d2e350d88369eda5528200ef5b
cocodemon <milanpaul212@outlook.com>
2026-06-14 14:56:28 +0530

    added response handler

--- cmd/httpserver/main.go
+++ cmd/httpserver/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"log"
+	"myhttp/internal/request"
+	"myhttp/internal/response"
 	"myhttp/internal/server"
 	"os"
 	"os/signal"
@@ -10,8 +12,47 @@ import (
 
 const port = 42069
 
+func handler(w *response.Response, req *request.Request) {
+	target := req.RequestLine.RequestTarget
+	w.Headers.Set("Content-Type", "text/html")
+	w.HandlerRes.StatusCode = 200
+	w.HandlerRes.Message = `<html>
+  <head>
+    <title>200 OK</title>
+  </head>
+  <body>
+    <h1>Success!</h1>
+    <p>Your request was an absolute banger.</p>
+  </body>
+</html>`
+	if target == "/yourproblem" {
+		w.HandlerRes.StatusCode = 400
+		w.HandlerRes.Message = `<html>
+  <head>
+    <title>400 Bad Request</title>
+  </head>
+  <body>
+    <h1>Bad Request</h1>
+    <p>Your request honestly kinda sucked.</p>
+  </body>
+</html>`
+	}
+	if target == "/myproblem" {
+		w.HandlerRes.StatusCode = 500
+		w.HandlerRes.Message = `<html>
+  <head>
+    <title>500 Internal Server Error</title>
+  </head>
+  <body>
+    <h1>Internal Server Error</h1>
+    <p>Okay, you know what? This one is on me.</p>
+  </body>
+</html>`
+	}
+}
+
 func main() {
-	server, err := server.Serve(port)
+	server, err := server.Serve(handler)
 	if err != nil {
 		log.Fatalf("Error starting server: %v", err)
 	}
